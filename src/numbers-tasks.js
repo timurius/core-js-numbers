beacd7fbc7389c38a626469680f029ff81e14b74
timurius <timur.yagudin.08@gmail.com>
2023-11-17 18:09:03 +0400

    feat: complete roundToPowerOfTen task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -175,8 +175,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(x, pow) {
+  return Math.round(x / 10 ** pow) * 10 ** pow;
 }
 
 /**
