a5f7420ed4dbdc78e559f259bacb0cc1121d01bc
timurius <timur.yagudin.08@gmail.com>
2023-11-18 17:39:44 +0400

    init: start working on numberToStringBase task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -347,8 +347,8 @@ function getSine(x) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(x, radix) {
+  return x.toString(radix);
 }
 
 /**
